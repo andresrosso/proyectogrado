e2f2913291cdcd2ee293c4760946a0c98a11c66d
andresrosso <andresrosso@gmail.com>
2012-02-03 11:55:04 -0500

    First commit

---
+++ ASimOB/src/org/arosso/test/Test.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package org.arosso.test;
+
+/**
+ * @author arosso
+ *
+ */
+public class Test {
+
+	/**
+	 * 
+	 */
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
